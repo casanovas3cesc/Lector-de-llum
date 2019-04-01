86613c2e70dff09b059a90596a566526836469c1
casanovas3cesc <cesc.casanovas3@gmail.com>
2019-04-01 17:14:02 +0200

    Update pr02.ino

--- pr02/pr02.ino
+++ pr02/pr02.ino
@@ -11,7 +11,9 @@
 const int ldr0 = A0;         // donar nom al pin A0 de l’Arduino
 const int ldr1 = A1;         // donar nom al pin A1 de l’Arduino
 const int ldr2 = A2;         // donar nom al pin A2 de l’Arduino
-
+const int led0 = 9;         // donar nom al pin A0 de l’Arduino
+const int led1 = 10;         // donar nom al pin A1 de l’Arduino
+const int led2 = 11;  
 
 int valLdr0, valLdr1, valLdr2;     // per guardar valors del ldrs
 
@@ -19,7 +21,9 @@ int valLdr0, valLdr1, valLdr2;     // per guardar valors del ldrs
 void setup()
 {
  Serial.begin(9600);
-
+pinMode(led0, OUTPUT);
+pinMode(led1, OUTPUT);
+pinMode(led2, OUTPUT);
 }
 
 //********** Loop *****************************************************************
@@ -29,14 +33,22 @@ void loop()
   Serial.print("ldr0 ");
   Serial.println(valLdr0);
   delay(400);
+  valLdr0 = map(valLdr0, 250, 800, 255, 0);
+  analogWrite(led0, valLdr0);
+  
   valLdr1 = analogRead(ldr1);
   Serial.print("ldr1 ");
   Serial.println(valLdr1);
    delay(400);
+   valLdr1 = map(valLdr0, 250,720, 255, 0);
+   analogWrite(led1, valLdr1);
+   
   valLdr2 = analogRead(ldr2);   // llegir valor ldr2
   Serial.print("ldr2 ");
   Serial.println(valLdr2);
   delay(400);
+  valLdr2 = map(valLdr2, 250, 750, 255, 0);
+  analogWrite (led2, valLdr2) ;
 
 }
 
